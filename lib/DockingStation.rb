c4cad3b876a2029ce64d378c614f57c7312d0815
Student <student@Makerss-MacBook-Pro.local>
2019-06-26 17:24:26 +0100

    Step11 added

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -1,27 +1,13 @@
 require_relative 'Bike.rb'
 
 class DockingStation
-
-  def initialize
-
-  end
-
+attr_reader :bike
   def release_bike
     Bike.new
   end
 
-  def dock_bike
-
-  #  @bike_storage
+  def dock_bike(bike)
+    @bike = bike
   end
 
-
 end
-
-#bike = Bike.new
-
-#station = DockingStation.new
-
-#station.dock_bike(bike)
-
-#station.view_storage
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -6,6 +6,14 @@ describe DockingStation do
       expect(bike).to be_working
     end
   it { is_expected.to respond_to(:dock_bike).with(1).argument }
-
-
+  it 'docks some bike' do
+    bike = Bike.new
+    expect(subject.dock_bike(bike)).to eq bike
   end
+  it { is_expected.to respond_to(:bike) }
+  it 'return docked bikes' do
+    bike = Bike.new
+    subject.dock_bike(bike)
+    expect(subject.bike).to eq bike
+  end 
+end
