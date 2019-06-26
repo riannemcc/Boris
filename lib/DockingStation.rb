808601b900f556eaada8aa0123918c84a2d0c815
Student <student@Makerss-MacBook-Pro.local>
2019-06-26 16:20:42 +0100

    stage 11

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -1,6 +1,27 @@
+require_relative 'Bike.rb'
+
 class DockingStation
 
-  def release_bike
+  def initialize
+
   end
 
+  def release_bike
+    Bike.new
+  end
+
+  def dock_bike
+
+  #  @bike_storage
+  end
+
+
 end
+
+#bike = Bike.new
+
+#station = DockingStation.new
+
+#station.dock_bike(bike)
+
+#station.view_storage
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -1,5 +1,11 @@
 require './lib/DockingStation.rb'
 describe DockingStation do
-  it 'release_bike' do 
-end
-end
+  it { is_expected.to respond_to :release_bike } #Check if there is a method
+    it 'releases a bike' do
+      bike = subject.release_bike
+      expect(bike).to be_working
+    end
+  it { is_expected.to respond_to(:dock_bike).with(1).argument }
+
+
+  end
